c62fe6f5c7b8ca233c11d1486560dc5daaa3e115
cahena <cahena@oregonstate.edu>
2024-02-10 16:30:42 -0800

    for loops working and bugs fixed.

--- main_2.0.cpp
+++ main_2.0.cpp
@@ -1,10 +1,7 @@
-//main_2.0.cpp
-//
-
 
 #include <iostream>
 #include <string>
-#include <vector>
+#include <vector> 
 
 using namespace std;
 
@@ -16,16 +13,23 @@ class Little
     vector<string> pref_survey;
     bool free = true;
     //Big* curr_pair1;
-    
+
     public:
 //CONSTRUCTOR:
     Little (const string& name_, const vector<string>& pref_survey_)
         : name(name_), pref_survey(pref_survey_) {
             cout<< "\nLittle constructor for " << name <<endl;
         }
-    
-//FUNCTIONS:
 
+//FUNCTIONS:
+string get_name()
+{
+    return name;
+}
+vector<string> get_pref_survey()
+{
+    return pref_survey;
+}
 //input: true or false.
 //result: Update free status.
     void change_free_status(bool new_status) {
@@ -34,8 +38,9 @@ class Little
 
 };
 
-struct Big
+class Big
 {
+    private:
 //VARIABLES:
     string name;
     vector<string> pref_survey;
@@ -50,6 +55,10 @@ struct Big
             cout<< "\nBig constructor for " << name <<endl;
         }
 //FUNCTIONS:
+string get_name()
+{
+    return name;
+}
 //input: Name that is being searched for.
 //output: Preference number of name.
     int get_pref_rank(string& little_name){
@@ -72,17 +81,38 @@ struct Big
 };
 
 
+void clean_data(vector<Big>& bigs, vector<Little>& littles)
+{
+    //not sure if needed yet
+}
 
 // Function that matchs Bigs and Littles:
 //input: Vector of all Bigs and vector of all Littles. 
-void match_bigs_littles(vector<Big>& bigs, vector<Little>& littles){
+void match_bigs_littles(vector<Big>& bigs, vector<Little>& littles)
+{
     cout<< "\nMatch function working!" <<endl;
+
+    bool new_changes_made = 1;
+    while(new_changes_made)
+    {
+        new_changes_made = 0; // Set to no changes have been made yet.
+
+        for (int i = 0; i < littles.size(); i++)
+        { // For each Little:
+            cout<< "\nLittle: " << littles[i].get_name() <<endl;
+            for (int j = 0; j < littles[i].get_pref_survey().size(); j++)
+            { // For each Big in Littles pref_survey:
+                cout<< "\nBig :" << bigs[j].get_name() <<endl;
+            }
+
+        }
+    }
+
 }
 
 
 int main()
 {
-//EXAMPLE FOR NOW, WILL BE READ IN LATER
     vector<Big> bigs = {{"Big1", {"Little3", "Little1", "Little2"}},
                         {"Big2", {"Little2", "Little1", "Little3"}},
                         {"Big3", {"Little1", "Little3", "Little2"}},
@@ -92,6 +122,8 @@ int main()
     vector<Little> littles = {{"Little1", {"Big2", "Big1", "Big3"}},
                               {"Little2", {"Big3", "Big1", "Big2"}},
                               {"Little3", {"Big2", "Big1", "Big3"}}};
+
+
 
     match_bigs_littles(bigs, littles);
 
